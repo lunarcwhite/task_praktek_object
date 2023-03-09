a90fc0dc98379313cfc602bfd1f9f5fdf24c98c4
lunarcwhite <rizkym058@gmail.com>
2023-03-09 20:38:06 +0700

    task object

---
+++ task-object.js
@@ -0,0 +1,57 @@
+// buat 3 object yang memiliki 5 properti
+// outputkan satu - persatu
+
+const kucing = {
+    kingdom: 'animalia',
+    phylum: 'chordata',
+    class: 'mamalia',
+    order: 'carnivora',
+    family: 'felidae',
+    genus: 'felis',
+    species: 'catus',  
+}
+console.log('~~~~~~ Klasifikasi Kucing ~~~~~~')
+console.log(`Kingdom: ${kucing.kingdom}`);
+console.log(`Phylum: ${kucing.phylum}`);
+console.log(`Class: ${kucing.class}`);
+console.log(`Order: ${kucing.order}`);
+console.log(`Family: ${kucing.family}`);
+console.log(`Genus: ${kucing.genus}`);
+console.log(`Species: ${kucing.species}`);
+console.log('~~~~~~ ------ ~~~~~~')
+const harimau = {
+    kingdom: 'animalia',
+    phylum: 'chordata',
+    class: 'mamalia',
+    order: 'carnivora',
+    family: 'felidae',
+    genus: 'panthera',
+    species: 'tigris',
+}
+console.log('~~~~~~ Klasifikasi Harimau ~~~~~~')
+console.log(`Kingdom: ${harimau.kingdom}`);
+console.log(`Phylum: ${harimau.phylum}`);
+console.log(`Class: ${harimau.class}`);
+console.log(`Order: ${harimau.order}`);
+console.log(`Family: ${harimau.family}`);
+console.log(`Genus: ${harimau.genus}`);
+console.log(`Species: ${harimau.species}`);
+console.log('~~~~~~ ------ ~~~~~~')
+const singa = {
+    kingdom: 'animalia',
+    phylum: 'chordata',
+    class: 'mamalia',
+    order: 'carnivora',
+    family: 'felidae',
+    genus: 'felis',
+    species: 'leo',
+}
+console.log('~~~~~~ Klasifikasi Singa ~~~~~~')
+console.log(`Kingdom: ${singa.kingdom}`);
+console.log(`Phylum: ${singa.phylum}`);
+console.log(`Class: ${singa.class}`);
+console.log(`Order: ${singa.order}`);
+console.log(`Family: ${singa.family}`);
+console.log(`Genus: ${singa.genus}`);
+console.log(`Species: ${singa.species}`);
+console.log('~~~~~~ ------ ~~~~~~')
